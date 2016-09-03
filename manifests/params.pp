fb7743d602da51ff89dbd002975cfac1561117ee
diegolima <diegolima.br@gmail.com>
2016-09-03 13:21:48 -0300

    Replace tabs with spaces

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,22 @@
 class playonlinux::params{
-	case $::osfamily {
-		'Debian': {
-		    $package = 'playonlinux'
-			case $::lsbdistid {
-				'Ubuntu': {
-					case $::lsbdistcodename {
-						'trusty','saucy','precise': {
-							$needs_repository = true
-							$repository_uri   = "http://deb.playonlinux.com"
-							$repository_key   = 'http://deb.playonlinux.com/public.gpg'
-                            $repository_key_id= '74F7358425EEB6176094C884E0F72778C4676186'
-						}
-						default: {
-							$needs_repository = false
-						}
-					}
-				}
-			}
-		}
-	}
+  case $::osfamily {
+  'Debian': {
+    $package = 'playonlinux'
+    case $::lsbdistid {
+      'Ubuntu': {
+        case $::lsbdistcodename {
+            'trusty','saucy','precise': {
+              $needs_repository = true
+              $repository_uri   = "http://deb.playonlinux.com"
+              $repository_key   = 'http://deb.playonlinux.com/public.gpg'
+              $repository_key_id= '74F7358425EEB6176094C884E0F72778C4676186'
+            }
+            default: {
+              $needs_repository = false
+            }
+          }
+        }
+      }
+    }
+  }
 }
