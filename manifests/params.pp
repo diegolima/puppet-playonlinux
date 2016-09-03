961c3d89997906c2dff92198a1b826ce451b48a0
diegolima <diegolima.br@gmail.com>
2016-09-03 13:29:12 -0300

    Fix uneeded double-quoted string

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class playonlinux::params{
         case $::lsbdistcodename {
             'trusty','saucy','precise': {
               $needs_repository = true
-              $repository_uri   = "http://deb.playonlinux.com"
+              $repository_uri   = 'http://deb.playonlinux.com'
               $repository_key   = 'http://deb.playonlinux.com/public.gpg'
               $repository_key_id= '74F7358425EEB6176094C884E0F72778C4676186'
             }
